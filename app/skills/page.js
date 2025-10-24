ac06a7dc6a2208b2a345b37b1d8f4b8d8bb5b3c2
Erkaaashajin <erdenesaikhan2011@gmail.com>
2025-10-24 20:19:31 +0800

    haha

--- app/skills/page.js
+++ app/skills/page.js
@@ -6,7 +6,14 @@ export default function Skills() {
 
   return (
     <div>
-      <img className="mt-[200px]" src="image/image.png" />
+      <div className="grid  grid-cols-2  gap-[50px]  sm:grid-cols-3">
+        {data.map((item, index) => (
+          <div className="w-[100px] h-[100px] bg-red-500">
+            <p className="text-white">{item.text}</p>
+            <Image src={item.img} height={100} width={100} alt="zurag" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
